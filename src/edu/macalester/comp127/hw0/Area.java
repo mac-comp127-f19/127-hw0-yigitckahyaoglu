801b265211fbe31e65995d515e3f89ed6e5b2cfe
yigitcankahyaoglu <Budasana12>
2020-01-23 15:49:08 -0600

    HW0 complete!!

--- src/edu/macalester/comp127/hw0/Area.java
+++ src/edu/macalester/comp127/hw0/Area.java
@@ -11,7 +11,8 @@ public class Area {
      */
     public static double getSquareArea(double sideLength) {
         // TODO: replace me with the correct formula
-        return sideLength * 1.0 * 42 +13;
+        return sideLength * sideLength;
+
     }
 
     /**
@@ -21,7 +22,7 @@ public class Area {
      */
     public static double getCircleArea(double radius) {
         // TODO: replace me with the correct formula
-        return radius * 1.0;
+        return radius * radius * Math.PI;
     }
 
 }
